c3937a5526bc5f0e167ea573bbda745d4b86d93e
rizkyputra098 <pratamaputra09876543@gmail.com>
2025-04-04 14:10:06 +0800

    pertama

---
+++ lib/clonescereenapp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Clonescereenapp extends StatefulWidget {
+  const Clonescereenapp({super.key});
+
+  @override
+  State<Clonescereenapp> createState() => _ClonescereenappState();
+}
+
+class _ClonescereenappState extends State<Clonescereenapp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text("data"),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone_app/clonescereenapp.dart';
+
+void main() {
+  runApp(Clonescereenapp());
+}
+
