6a5e1403cabd2e21a8879b8884ae5b74abf71ed9
rizkyputra098 <pratamaputra09876543@gmail.com>
2025-04-06 21:52:02 +0800

    done

--- lib/clonescereenapp.dart
+++ lib/clonescereenapp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_clone_app/data.dart';
 
 class Clonescereenapp extends StatelessWidget {
   const Clonescereenapp({super.key});
@@ -29,7 +30,7 @@ class SongPlayListScreen extends StatelessWidget {
           title: const Text(
             "Spotify Playlist",
             style: TextStyle(
-              fontSize: 24,
+              fontSize: 30,
               color: Colors.white,
               fontWeight: FontWeight.bold,
             ),
@@ -38,7 +39,7 @@ class SongPlayListScreen extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(
             horizontal: 12.0,
-            vertical: 4.0,
+            vertical: 8.0,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -47,40 +48,34 @@ class SongPlayListScreen extends StatelessWidget {
               Center(
                 child: Image.asset(
                   "assets/images/playlist_cover.jpg",
-                  width: 200,
-                  height: 200,
+                  width: 350,
+                  height: 350,
                 ),
               ),
-              const SizedBox(
-                height: 16.0,
-              ),
+              SizedBox(height: 16.0),
               Center(
                 child: Text(
                   "My Playlist",
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 25,
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 8.0,
-              ),
+              const SizedBox(height: 12.0),
               const Text(
                 "ini isinya lagu-lagu melow bukan karena galau, tapi karena emang suka lagu melow dan selow",
                 style: TextStyle(
-                  fontSize: 10,
+                  fontSize: 18,
                   color: Colors.white,
                 ),
               ),
-              const SizedBox(
-                height: 8.0,
-              ),
+              const SizedBox(height: 8.0),
               Row(
                 children: [
                   CircleAvatar(
-                    radius: 16,
+                    radius: 20,
                     backgroundImage: AssetImage("assets/images/profile.jpg"),
                   ),
                   SizedBox(height: 20.0, width: 12.0),
@@ -94,9 +89,7 @@ class SongPlayListScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(
-                height: 10,
-              ),
+              SizedBox(height: 10),
               Row(
                 children: [
                   Icon(
@@ -115,6 +108,119 @@ class SongPlayListScreen extends StatelessWidget {
                     ),
                   ),
                 ],
+              ),
+              SizedBox(height: 8.0),
+              Row(
+                children: [
+                  Container(
+                    width: 35,
+                    height: 50,
+                    padding: EdgeInsets.all(2.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 2.0,
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8.0),
+                      ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            "assets/images/cover.jpg",
+                          ),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Icon(
+                    Icons.arrow_circle_down,
+                    color: Colors.grey,
+                    size: 32,
+                  ),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  Icon(
+                    Icons.person_add,
+                    color: Colors.grey,
+                    size: 32,
+                  ),
+                  SizedBox(width: 24.0),
+                  Icon(
+                    Icons.more_horiz,
+                    color: Colors.grey,
+                    size: 32,
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.shuffle,
+                    color: Colors.grey,
+                    size: 32,
+                  ),
+                  SizedBox(width: 12),
+                  Icon(
+                    Icons.play_circle,
+                    color: Color(0xff1db954),
+                    size: 52,
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+
+              Expanded(
+                child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            songDataList[index].cover,
+                            width: 50,
+                            height: 50,
+                          ),
+                          SizedBox(
+                            width: 16.0,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                songDataList[index].deskription,
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                songDataList[index].tittle,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      height: 12.0,
+                    );
+                  },
+                  itemCount: songDataList.length,
+                ),
               )
             ],
           ),
--- lib/data.dart
+++ lib/data.dart
@@ -1,1 +1,74 @@
-class Data {}
+class SongData {
+  final String tittle;
+  final String deskription;
+  final String cover;
+
+  const SongData({
+    required this.tittle,
+    required this.deskription,
+    required this.cover,
+  });
+}
+
+var songDataList = [
+  SongData(
+    tittle: "Doa Terimakasih",
+    deskription: "Nosstress",
+    cover: "assets/images/playlists/doa_terima_kasih.jpeg",
+  ),
+  SongData(
+    tittle: "Esay On Me",
+    deskription: "Adele",
+    cover: "assets/images/playlists/easy_on_me.jpeg",
+  ),
+  SongData(
+    tittle: "Exile",
+    deskription: "Taylor Swift, Bon Iver",
+    cover: "assets/images/playlists/exile.jpeg",
+  ),
+  SongData(
+    tittle: "Here WIth ME",
+    deskription: "d4dv",
+    cover: "assets/images/playlists/here_with_me.jpeg",
+  ),
+  SongData(
+    tittle: "Imagination",
+    deskription: "Shwan Mendes",
+    cover: "assets/images/playlists/imagination.jpeg",
+  ),
+  SongData(
+    tittle: "Love",
+    deskription: "wave to earth",
+    cover: "assets/images/playlists/love.jpeg",
+  ),
+  SongData(
+    tittle: "Membasuh",
+    deskription: "Hindia, Rare Sekar",
+    cover: "assets/images/playlists/membasuh.jpeg",
+  ),
+  SongData(
+    tittle: "Oceans & Engine",
+    deskription: "NIKI",
+    cover: "assets/images/playlists/oceans_engine.jpeg",
+  ),
+  SongData(
+    tittle: "One More Light",
+    deskription: "Linkin Part",
+    cover: "assets/images/playlists/one_more_light.jpeg",
+  ),
+  SongData(
+    tittle: "Rehat",
+    deskription: "Kunto Aji",
+    cover: "assets/images/playlists/rehat.jpeg",
+  ),
+  SongData(
+    tittle: "Satu BUlan",
+    deskription: "Bernadya",
+    cover: "assets/images/playlists/satu_bulan.jpeg",
+  ),
+  SongData(
+    tittle: "WijayaKusuma",
+    deskription: "Ardhito Pramana",
+    cover: "assets/images/playlists/wijayakusuma.jpeg",
+  ),
+];
